83235ad258a827cdcdfc86340114bf0ecf455a17
orestk98 <60813740+orestk98@users.noreply.github.com>
2020-07-08 23:22:16 +0200

    first commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db ={
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2020-07-08T16:39:37.233Z',
+      likeCount: 5,
+      commentsCount: 2
+    }
+  ]
+}
