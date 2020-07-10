d95d81bbebdcf75c75d27b9a76933aeec6ed45af
orestk98 <60813740+orestk98@users.noreply.github.com>
2020-07-10 19:43:46 +0200

    organized folders saved

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,4 +1,4 @@
-const {db} = require ('../util/admin');
+const {admin, db} = require ('../util/admin');
 const firebase = require('firebase');
 const config = require('../util/config');
 firebase.initializeApp(config);
@@ -89,4 +89,33 @@ firebase.auth().signInWithEmailAndPassword(user.email, user.password).then(data=
 }
 })
 
+}
+
+
+exports.uploadImage = (req, res) => {
+const BusBoy = require('busboy');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+const busboy = new BusBoy({headers: req.headers});
+
+let imageFileName;
+let imageToBeUploaded = {};
+
+busboy.on('file', (fieldname, file, filename, encoding, mimetype)=>{
+console.log(fieldname);
+console.log(filename);
+console.log(mimetype);
+
+
+
+  const imageExtension = filename.split('.')[filename.split('.').length -1];
+const imageFileName = `${Math.round(Math.random()*1000000000)}.${imageExtension}`
+const filepath = path.join(os.tmpdir(), imageFileName);
+imageToBeUploaded = {filepath, mimetype};
+
+file.pipe(fs.createWriteStream(filepath));
+});
+  busboy.on('finish')
 }
