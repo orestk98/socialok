29b42c06c365e532e75e35ac85d6d319430649ca
orestk98 <60813740+orestk98@users.noreply.github.com>
2020-07-10 19:42:35 +0200

    organized folders

--- functions/index.js
+++ functions/index.js
@@ -1,111 +1,25 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+
 const app = require('express')();
-admin.initializeApp();
+
+
+const FBAuth = require('./util/FbAuth')
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+const {getAllScreams, postOneScream} = require ('./handlers/screams')
+
+const { signup, login, uploadImage } = require ('./handlers/users')
 
 
-var config = {
-    apiKey: "AIzaSyBoi7AEs5c6dVUZDxlC16geLKfPA1MKeuE",
-    authDomain: "socialok.firebaseapp.com",
-    databaseURL: "https://socialok.firebaseio.com",
-    projectId: "socialok",
-    storageBucket: "socialok.appspot.com",
-    messagingSenderId: "936502606066",
-    appId: "1:936502606066:web:effa1b4b61aa010331b86b",
-    measurementId: "G-E0G8RKGZV9"
-  };
-
-
-const firebase = require('firebase');
-firebase.initializeApp(config);
-
-const db = admin.firestore();
-
-app.get('/screams',(req,res)=>{
-  db.collection('screams').orderBy('createdAt','desc').get().then(data=>{
-    let screams =[];
-    data.forEach(doc=>{
-      screams.push({
-        screamId: doc.id,
-        body: doc.data().body,
-        userHandle: doc.data().userHandle,
-        createdAt: doc.data().createdAt
-      });
-    });
-    return res.json(screams);
-  }).catch(err => console.log(err))
-})
-
-
-app.post('/scream', (req,res)=>{
-  const newScream = {
-    body: req.body.body,
-    userHandle: req.body.userHandle,
-    createdAt: new Date().toISOString()
-  };
-
-  db.collection('screams').add(newScream)
-  .then(doc=>{
-      res.json({message: `document ${doc.id} created successfully`});
-    })
-    .catch(err=>{
-    res.status(500).json({error: 'something went wrong'});
-    console.error(err);
-  });
-});
-
-// Signup route
-
-app.post('/signup', (req, res)=>{
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle
-  }
-let token;
-  db.doc(`/users/${newUser.handle}`).get()
-  .then(doc=>{
-    if(doc.exists){
-      return res.status(400).json({ handle: 'this handle is already taken.'})
-    }else{
-      return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
-    }
-  }).then(data=>{
-    userId = data.user.uid;
-    return data.user.getIdToken();
-  }).then(idToken=>{
-    token =idToken;
-  const userCredentials ={
-    handle: newUser.handle,
-    email: newUser.email,
-    createdAt: new Date().toISOString(),
-    userId
-  }
-return  db.doc(`/users/${newUser.handle}`).set(userCredentials);
-}).then(()=>{
-  return res.status(201).json({token});
-})
-  .catch(err=>{
-    console.error(err);
-    if(err.code === 'auth/email-already-in-use'){
-      return res.status(400).json({ email:'Email is already used'})
-    }else{
-      return res.status(500).json({error: err.code})
-
-    }
-  })
-  //
-  // firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password).then(data=>{
-  //   return res.status(201).json({message: `user ${data.user.uid} signed up succesfully`}).catch(err=>{
-  //     console.error(err);
-  //     return res.status(500).json({ error: err.code})
-  //   })
-  // })
-})
+//scream routes
+app.get('/screams', getAllScreams)
+app.post('/scream',FBAuth, postOneScream);
+//users ruotes
+app.post('/signup', signup);
+app.post('/login', login);
+app.post('/user/image', uploadImage)
 
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
