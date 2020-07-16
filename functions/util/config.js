4df849db7a7e0f20a84b3865b91bccc805322a49
orestk98 <60813740+orestk98@users.noreply.github.com>
2020-07-16 15:13:13 +0200

    update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    apiKey: "AIzaSyBoi7AEs5c6dVUZDxlC16geLKfPA1MKeuE",
-    authDomain: "socialok.firebaseapp.com",
-    databaseURL: "https://socialok.firebaseio.com",
-    projectId: "socialok",
-    storageBucket: "socialok.appspot.com",
-    messagingSenderId: "936502606066",
-    appId: "1:936502606066:web:effa1b4b61aa010331b86b",
-    measurementId: "G-E0G8RKGZV9"
+   //secret data
   };
